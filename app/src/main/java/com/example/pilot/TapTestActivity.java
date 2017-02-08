da34fd14871a8d15506b4b81f3cd0ce1c660a266
jagluck <jagluck@terpmail.umd.edu>
2017-02-08 18:02:29 -0500

    added timer to taptest

--- app/src/main/java/com/example/pilot/TapTestActivity.java
+++ app/src/main/java/com/example/pilot/TapTestActivity.java
@@ -15,6 +15,7 @@ public class TapTestActivity extends AppCompatActivity {
     public boolean canTapScreen = true;
     public Integer leftHand = 0;
     public Integer rightHand = 0;
+    public Integer timeCount = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +26,20 @@ public class TapTestActivity extends AppCompatActivity {
     public void countTap(View view) {
         if (canTapScreen) {
             final TextView textViewToChange = (TextView) findViewById(R.id.txtInstruction);
+            final TextView textViewToChange2 = (TextView) findViewById(R.id.txtTime);
             if (!startTest) {
                 startTest = true;
                 new CountDownTimer(10000, 1000) {
-                    public void onTick(long pie) {
+                    public void onTick(long millisUntilFinished) {
+                        timeCount--;
+                        textViewToChange2.setText(timeCount.toString());
                     }
 
                     public void onFinish() {
                         if (leftHandTest) {
                             textViewToChange.setText("Get ready for your right hand!");
+                            textViewToChange2.setText("10");
+                            timeCount = 10;
                             canTapScreen = false;
                             leftHandTest = false;
 
@@ -48,6 +54,7 @@ public class TapTestActivity extends AppCompatActivity {
 
                         } else {
                             textViewToChange.setText("Results: \n Left Hand: " + leftHand.toString() + "\n Right Hand: " + rightHand.toString());
+                            textViewToChange2.setText("Done");
                             canTapScreen = false;
                         }
                         startTest = false;
