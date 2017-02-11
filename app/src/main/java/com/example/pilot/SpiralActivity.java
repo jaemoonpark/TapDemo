cce6af4aec044127584736d9bb635c56ad31c5ae
Ben McHugh <benlmchugh8@gmail.com>
2017-02-11 16:51:14 -0500

    Added some variables and started a method to prepare the app to draw

--- app/src/main/java/com/example/pilot/SpiralActivity.java
+++ app/src/main/java/com/example/pilot/SpiralActivity.java
@@ -1,13 +1,29 @@
 package com.example.pilot;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SpiralActivity extends AppCompatActivity {
+    private Path tracePath;
+    private Paint paint;
+    private Paint canvasPaint;
+    private int redPaint = 0xff0000;
+    private Canvas drawCanvas;
+    private Bitmap canvasBitmap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spiral);
+    }
+
+    protected void drawPrep(){
+        tracePath = new Path();
+        paint = new Paint();
     }
 }
