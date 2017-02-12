2477f73c52f324acdfd90ae9e2eef110f1dc3815
Jada <jwashin7@terpmail.umd.edu>
2017-02-12 16:16:54 -0500

    Added stuff to SpiralActvitiy onCreate() to add in SpiralView

--- app/src/main/java/com/example/pilot/SpiralActivity.java
+++ app/src/main/java/com/example/pilot/SpiralActivity.java
@@ -2,6 +2,7 @@ package com.example.pilot;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
 
 public class SpiralActivity extends AppCompatActivity {
 
@@ -10,6 +11,11 @@ public class SpiralActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SpiralView draw = new SpiralView(null, null);
+        RelativeLayout lay = new RelativeLayout(this);
+
+        lay.addView(draw);
         setContentView(R.layout.activity_spiral);
     }
 
