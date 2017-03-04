d0c8a6add630562c93d2d933dbd2ece6cbb02a8f
Ben McHugh <benlmchugh8@gmail.com>
2017-03-04 13:40:22 -0500

    Added the BubbleView and a red bubble will appear when you click the bubble activity button. Still need to add click functionality to the bubble and record the reaction time as well

---
+++ app/src/main/java/com/example/pilot/BubbleView.java
@@ -0,0 +1,29 @@
+package com.example.pilot;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+
+public class BubbleView extends View {
+    Paint paint = null;
+
+    public BubbleView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+        paint = new Paint();
+        paint.setColor(Color.RED);
+    }
+
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+        int x = getWidth();
+        int y = getHeight();
+        int radius = 50;
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(x/2,y/2,radius,paint);
+    }
+}
