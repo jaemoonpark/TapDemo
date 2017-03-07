df5eb01981cbd84502b8e7ab38829acf42053713
Ben McHugh <benlmchugh8@gmail.com>
2017-03-07 07:43:44 -0500

    Only touches on or very near the circle (gotta help out the old people a little) will count.  Tried to add an average time counter but it's not really working yet.

--- app/src/main/java/com/example/pilot/BubbleView.java
+++ app/src/main/java/com/example/pilot/BubbleView.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
@@ -20,8 +21,10 @@ public class BubbleView extends View {
     int circleX;
     int circleY;
     Random r = new Random();
-
-
+    protected long startTime;
+    protected long endTime;
+    protected long averageTime = 0;
+    int numTrials = 0;
     public BubbleView(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
         paint = new Paint();
@@ -33,29 +36,63 @@ public class BubbleView extends View {
 
     protected void onDraw(Canvas canvas){
         super.onDraw(canvas);
-        int x = getWidth();
-        int y = getHeight();
+        if(numTrials < 20) {
+            int x = getWidth();
+            int y = getHeight();
 
-        circleX = x/2;
-        circleY = y/2;
-        int radius = 50;
-        paint.setStyle(Paint.Style.FILL);
-        canvas.drawCircle(circleX,circleY,radius,paint);
+            circleX = x / 2;
+            circleY = y / 2;
+            int radius = 50;
+            paint.setStyle(Paint.Style.FILL);
+            canvas.drawCircle(circleX, circleY, radius, paint);
+            startTime = System.currentTimeMillis();
+            if (isSwitched()) {
+                circleX = r.nextInt(x - 1);
+                circleY = r.nextInt(y - 1);
+                paint.setColor(Color.RED);
+                canvas.drawColor(Color.WHITE);
+                canvas.drawCircle(circleX, circleY, radius, paint);
+                num2 = num;
 
-        if(isSwitched()) {
-            circleX = r.nextInt(x - 1);
-            circleY = r.nextInt(y - 1);
-            paint.setColor(Color.RED);
-            canvas.drawColor(Color.WHITE);
-            canvas.drawCircle(circleX,circleY,radius,paint);
-            num2 = num;
+            }
+        }else{
+            Log.v("avgTime", Double.toString(averageTime) + " seconds");
         }
     }
 
+    private double getCurrentCircleX(){
+        return circleX;
+    }
+
+    private double getCurrentCircleY(){
+        return circleY;
+    }
+
+    private boolean isWithin(double circle, double touch){
+        return Math.abs(circle - touch) <= 70;
+    }
+
     public boolean onTouchEvent(MotionEvent event) {
-        changeNum();
-        invalidate();
-        return true;
+        double touchX = event.getX();
+        double touchY = event.getY();
+
+
+        if(isWithin(getCurrentCircleX(), touchX) && isWithin(getCurrentCircleY(), touchY)) {
+
+            numTrials++;
+            changeNum();
+            invalidate();
+            endTime = System.currentTimeMillis();
+            if (averageTime == 0) {
+                averageTime = endTime;
+            } else {
+                averageTime = (averageTime + endTime) / 2;
+            }
+            return true;
+
+        }else{
+            return true;
+        }
     }
 
     public boolean isSwitched() {
