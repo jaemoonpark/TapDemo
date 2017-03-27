d3b8932e6c47004e5176fb1e45e802dc387d879b
Jaemoon <jaemoon@jaemoonpark.com>
2017-03-27 17:41:43 -0400

    implement partial arm curl count

--- app/src/main/java/com/example/pilot/ArmActivity.java
+++ app/src/main/java/com/example/pilot/ArmActivity.java
@@ -17,8 +17,11 @@ public class ArmActivity extends AppCompatActivity implements SensorEventListene
     private Sensor mSensor;
     private int tapCount = 3;
     private int curlCount = 0;
+    private int curlAttempt = 0;
+    private boolean attemptCurl = false;
     private boolean countCurls = false;
     private boolean initiateOnce = false;
+    private boolean startTest = false;
     boolean getInitialValues;
     private TextView txtInstructions;
 
@@ -41,34 +44,47 @@ public class ArmActivity extends AppCompatActivity implements SensorEventListene
             txtInstructions.setText("Make sure you're holding your phone/tablet in landscape mode. Ensure your arm is laid out comfortably on a flat surface. \nTo begin, tap " + tapCount + " times with the arm that is not being curled.");
             if (tapCount == 0) {
                 countCurls = true;
+                attemptCurl = true;
                 initiateOnce = true;
-                txtInstructions.setText("You have completed 0 curls.");
+                startTest = true;
+                txtInstructions.setText("You have attempted 0 curls. You have completed 0 curls.");
             }
         }
     }
     @Override
     public final void onSensorChanged(SensorEvent event) {
-        // The light sensor returns a single value.
-        // Many sensors return 3 values, one for each axis.
-        float xAxis = event.values[0];
-        float zAxis = event.values[2];
-        if(countCurls) {
-            //debug garlic bread
-//            float xAxis = event.values[0];
-//            float zAxis = event.values[2];
-            //set x axis for 9  and zaxis -2 for a more strict 90 degree arm curl
-            if(xAxis > 8 && zAxis < -1){
-                curlCount += 1;
-                txtInstructions.setText("You have completed "+ curlCount + " curls.");
-                countCurls = false;
-            }
-            System.out.println("X axis is " + xAxis);
-            System.out.println("Z axis is " + zAxis);
-            System.out.println("--------------------");
-        }else{
-            //indication arm got laid back down
-            if(xAxis < 0 && zAxis > 8){
-                countCurls = true;
+        if(startTest) {
+            float xAxis = event.values[0];
+            float zAxis = event.values[2];
+            if (countCurls) {
+
+                //attempted arm curl
+                if (attemptCurl && xAxis > 2) {
+                    curlAttempt += 1;
+                    txtInstructions.setText("You have attempted " + curlAttempt + " curls. You have completed " + curlCount + " curls.");
+                    attemptCurl = false;
+                }
+                //attempted and did not finish
+                if (!attemptCurl && xAxis < 0 && zAxis > 8) {
+                    countCurls = false;
+                }
+
+                //a completed arm test
+                //set x axis for 9  and zaxis -2 for a more strict 90 degree arm curl
+                if (xAxis > 8 && zAxis < -1) {
+                    curlCount += 1;
+                    txtInstructions.setText("You have attempted " + curlAttempt + " curls. You have completed " + curlCount + " curls.");
+                    countCurls = false;
+                }
+                System.out.println("X axis is " + xAxis);
+                System.out.println("Z axis is " + zAxis);
+                System.out.println("--------------------");
+            } else {
+                //indication arm got laid back down
+                if (xAxis < 0 && zAxis > 8) {
+                    countCurls = true;
+                    attemptCurl = true;
+                }
             }
         }
 
