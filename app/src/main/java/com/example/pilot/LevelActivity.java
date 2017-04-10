1ef6040fcaf0aca2e30815c6c7b96e834a39b811
Jaemoon <jaemoon@jaemoonpark.com>
2017-04-10 00:31:49 -0400

    pushing to test sheet

--- app/src/main/java/com/example/pilot/LevelActivity.java
+++ app/src/main/java/com/example/pilot/LevelActivity.java
@@ -1,6 +1,8 @@
 package com.example.pilot;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -8,7 +10,9 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -18,7 +22,7 @@ import cmsc436.tharri16.googlesheetshelper.CMSC436Sheet;
 
 
 /* Learning how to gather sensor data that senses movement of device */
-public class LevelActivity extends AppCompatActivity implements SensorEventListener {
+public class LevelActivity extends AppCompatActivity implements SensorEventListener, CMSC436Sheet.Host {
     private SensorManager mSensorManager;
     private Sensor mSensor;
     private float xDraw;
@@ -124,10 +128,7 @@ public class LevelActivity extends AppCompatActivity implements SensorEventListe
             return "A";
         }
     }
-    private void sendResultToSheet(){
-        //sheet = new CMSC436Sheet(this, getString(R.string.app_name), getString(R.string.CMSC436Sheet_spreadsheet_id_test_sheet));
 
-    }
 
     public void startBullseyeTest(View view){
         if(!testStarted){
@@ -153,5 +154,53 @@ public class LevelActivity extends AppCompatActivity implements SensorEventListe
             }.start();
         }
 
+    }
+
+
+    private void sendResultToSheet(){
+        sheet = new CMSC436Sheet(this, getString(R.string.app_name), getString(R.string.CMSC436Sheet_spreadsheet_id));
+        sheet.writeData(CMSC436Sheet.TestType.LH_TAP, "folder", CMSC436Sheet.unixToSheetsEpoch(System.currentTimeMillis()));
+        sheet.writeData(CMSC436Sheet.TestType.LH_TAP, "pumpkin", 1.23f);
+    }
+    /* neccessary? */
+    @Override
+    public void onRequestPermissionsResult (int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        sheet.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        sheet.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public int getRequestCode(CMSC436Sheet.Action action) {
+        switch (action) {
+            case REQUEST_ACCOUNT_NAME:
+                return 2;
+            case REQUEST_AUTHORIZATION:
+                return 2;
+            case REQUEST_PERMISSIONS:
+                return 2;
+            case REQUEST_PLAY_SERVICES:
+                return 2;
+            default:
+                return -1; // boo java doesn't know we exhausted the enum
+        }
+    }
+
+    @Override
+    public Activity getActivity() {
+        return this;
+    }
+
+    @Override
+    public void notifyFinished(Exception e) {
+        if (e != null) {
+            throw new RuntimeException(e); // just to see the exception easily in logcat
+        }
+
+        Log.i(getClass().getSimpleName(), "Done");
     }
 }
