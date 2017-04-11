ce216bbc36f4d85a2e6d614c4da7d107b1e7095c
Ben McHugh <benlmchugh8@gmail.com>
2017-04-10 22:51:28 -0400

    Aight the start sound and the end sound both play bless up. Still need the three trials with the 5 second break in between

--- app/src/main/java/com/example/pilot/BubbleActivity.java
+++ app/src/main/java/com/example/pilot/BubbleActivity.java
@@ -1,14 +1,21 @@
 package com.example.pilot;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
-public class BubbleActivity extends AppCompatActivity {
+import cmsc436.tharri16.googlesheetshelper.CMSC436Sheet;
+
+public class BubbleActivity extends AppCompatActivity implements CMSC436Sheet.Host{
     public static TextView textView;
+    private CMSC436Sheet sheet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +31,50 @@ public class BubbleActivity extends AppCompatActivity {
         textView.setText("");
     }
 
+    protected void sendResultToSheet(double leftHand, double rightHand){
+        sheet = new CMSC436Sheet(this, getString(R.string.app_name), getString(R.string.CMSC436Sheet_spreadsheet_id));
+        sheet.writeData(CMSC436Sheet.TestType.LH_POP, "t01p01", (float) leftHand);
+        sheet.writeData(CMSC436Sheet.TestType.RH_POP, "t01p01", (float) rightHand);
+    }
+    /* neccessary? */
+    @Override
+    public void onRequestPermissionsResult (int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        sheet.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        sheet.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public int getRequestCode(CMSC436Sheet.Action action) {
+        switch (action) {
+            case REQUEST_ACCOUNT_NAME:
+                return 2;
+            case REQUEST_AUTHORIZATION:
+                return 2;
+            case REQUEST_PERMISSIONS:
+                return 2;
+            case REQUEST_PLAY_SERVICES:
+                return 2;
+            default:
+                return -1; // boo java doesn't know we exhausted the enum
+        }
+    }
+
+    @Override
+    public Activity getActivity() {
+        return this;
+    }
+
+    @Override
+    public void notifyFinished(Exception e) {
+        if (e != null) {
+            throw new RuntimeException(e); // just to see the exception easily in logcat
+        }
+
+        Log.i(getClass().getSimpleName(), "Done");
+    }
 }
