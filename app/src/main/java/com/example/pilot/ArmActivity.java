3a59fc9c22f14504cb7dbb9a2ca3e9825e00240d
jagluck <jagluck@terpmail.umd.edu>
2017-04-11 00:12:22 -0400

    added sheet implementation to arm and head tests

--- app/src/main/java/com/example/pilot/ArmActivity.java
+++ app/src/main/java/com/example/pilot/ArmActivity.java
@@ -1,18 +1,24 @@
 package com.example.pilot;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.CountDownTimer;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-public class ArmActivity extends AppCompatActivity implements SensorEventListener {
+import cmsc436.tharri16.googlesheetshelper.CMSC436Sheet;
+
+public class ArmActivity extends AppCompatActivity implements SensorEventListener, CMSC436Sheet.Host {
     private SensorManager mSensorManager;
     private Sensor mSensor;
     private int tapCount = 3;
@@ -27,7 +33,7 @@ public class ArmActivity extends AppCompatActivity implements SensorEventListene
     private boolean startTest = false;
     boolean getInitialValues;
     private TextView txtInstructions;
-
+    private CMSC436Sheet sheet;
 
 
     @Override
@@ -95,6 +101,7 @@ public class ArmActivity extends AppCompatActivity implements SensorEventListene
                         time = (stopTime - startTime);
                         time = time /1000;
                         txtInstructions.setText("Test is over. \nYou have attempted " + curlAttempt + " curls. You have completed " + curlCount + " curls." + " It took you " + Double.toString(time) + " seconds");
+                        sendResultToSheet(curlAttempt, curlCount);
                     }
                 }
 
@@ -132,4 +139,50 @@ public class ArmActivity extends AppCompatActivity implements SensorEventListene
         mSensorManager.unregisterListener(this);
     }
 
+    private void sendResultToSheet(double leftHand, double rightHand){
+        sheet = new CMSC436Sheet(this, getString(R.string.app_name), getString(R.string.CMSC436Sheet_spreadsheet_id));
+        sheet.writeData(CMSC436Sheet.TestType.LH_CURL, "t01p01", (float) leftHand);
+        sheet.writeData(CMSC436Sheet.TestType.RH_CURL, "t01p01", (float) rightHand);
+    }
+    /* neccessary? */
+    @Override
+    public void onRequestPermissionsResult (int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        sheet.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        sheet.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public int getRequestCode(CMSC436Sheet.Action action) {
+        switch (action) {
+            case REQUEST_ACCOUNT_NAME:
+                return 2;
+            case REQUEST_AUTHORIZATION:
+                return 2;
+            case REQUEST_PERMISSIONS:
+                return 2;
+            case REQUEST_PLAY_SERVICES:
+                return 2;
+            default:
+                return -1; // boo java doesn't know we exhausted the enum
+        }
+    }
+
+    @Override
+    public Activity getActivity() {
+        return this;
+    }
+
+    @Override
+    public void notifyFinished(Exception e) {
+        if (e != null) {
+            throw new RuntimeException(e); // just to see the exception easily in logcat
+        }
+
+        Log.i(getClass().getSimpleName(), "Done");
+    }
 }
--- app/src/main/java/com/example/pilot/HeadActivity.java
+++ app/src/main/java/com/example/pilot/HeadActivity.java
@@ -1,6 +1,8 @@
 package com.example.pilot;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -8,15 +10,19 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.CountDownTimer;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class HeadActivity extends AppCompatActivity implements SensorEventListener {
+import cmsc436.tharri16.googlesheetshelper.CMSC436Sheet;
+
+public class HeadActivity extends AppCompatActivity implements SensorEventListener, CMSC436Sheet.Host {
     private SensorManager mSensorManager;
     private Sensor mSensor;
     private TextView instructions;
@@ -38,6 +44,7 @@ public class HeadActivity extends AppCompatActivity implements SensorEventListen
 
     private MediaPlayer startTest;
     private MediaPlayer testComplete;
+    private CMSC436Sheet sheet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -171,5 +178,53 @@ public class HeadActivity extends AppCompatActivity implements SensorEventListen
             }.start();
         }
 
+    }
+
+    private void sendResultToSheet(){
+        //there is not implementation for head yet
+      ///  sheet = new CMSC436Sheet(this, getString(R.string.app_name), getString(R.string.CMSC436Sheet_spreadsheet_id));
+      //  sheet.writeData(CMSC436Sheet.TestType., "t01p01", leftHand);
+     //   sheet.writeData(CMSC436Sheet.TestType.RH_TAP, "t01p01", rightHand);
+    }
+    /* neccessary? */
+    @Override
+    public void onRequestPermissionsResult (int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        sheet.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        sheet.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public int getRequestCode(CMSC436Sheet.Action action) {
+        switch (action) {
+            case REQUEST_ACCOUNT_NAME:
+                return 2;
+            case REQUEST_AUTHORIZATION:
+                return 2;
+            case REQUEST_PERMISSIONS:
+                return 2;
+            case REQUEST_PLAY_SERVICES:
+                return 2;
+            default:
+                return -1; // boo java doesn't know we exhausted the enum
+        }
+    }
+
+    @Override
+    public Activity getActivity() {
+        return this;
+    }
+
+    @Override
+    public void notifyFinished(Exception e) {
+        if (e != null) {
+            throw new RuntimeException(e); // just to see the exception easily in logcat
+        }
+
+        Log.i(getClass().getSimpleName(), "Done");
     }
 }
