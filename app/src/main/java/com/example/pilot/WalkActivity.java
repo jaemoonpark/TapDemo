c2e1f6d58819cd297ab7216588b98946396d1b9e
Ben McHugh <benlmchugh8@gmail.com>
2017-04-17 23:06:28 -0400

    Added Button and walkTest method

--- app/src/main/java/com/example/pilot/WalkActivity.java
+++ app/src/main/java/com/example/pilot/WalkActivity.java
@@ -2,6 +2,7 @@ package com.example.pilot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WalkActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class WalkActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_walk);
+    }
+
+    public void walkTest(View view){
+        
     }
 }
